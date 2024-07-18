922bc2c55a642898ccde290816b33317edab7c16
Chitrangi Mestry <chitrangidmestrymbp@gmail.com>
2024-07-19 03:14:57 +0530

    DB connected + made express backend

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Chitrangi</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-indigo-700">Chitrangi</span>
+            <span className="text-indigo-900">Estate</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
